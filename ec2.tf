2b230085aa49d716a178f8d2d13547bc5d508324
bhagth <89497901+bhagth@users.noreply.github.com>
2022-02-21 13:03:24 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "us-east-1"
 }
  resource "aws instance" "ec2" {
-   ami = "ami-0f7691f59fd7c47af"
+   ami = "ami-033b95fb8079dc481"
    instance_type = "t2.micro"
    key_name = "jenkinssurver"
  }
